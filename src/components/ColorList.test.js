69edeb298a295591696e545fdc554d80ed53f245
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-06-18 14:15:34 -0700

    Builds Color component tests

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { render, screen} from "@testing-library/react";
-import ColorList from './ColorList';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ColorList from "./ColorList";
+
+const testColors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  },
+];
 
 test("Renders an empty list of colors without errors", () => {
+  render(<ColorList colors={[]} />);
 });
 
 test("Renders a list of colors without errors", () => {
+  render(<ColorList colors={testColors} />);
 });
 
-test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
-});
+test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {});
